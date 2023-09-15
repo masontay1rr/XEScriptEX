b8645bd4d28155e79bd0d52357f285f3060928be
HOMYCODEY <94492486+masontay1rr@users.noreply.github.com>
2023-09-15 17:27:08 -0500

    Without Terminal

--- X-E ScriptEX.js
+++ X-E ScriptEX.js
@@ -1,35 +1,29 @@
-(function () {
+(function(){
   function executeScript() {
     try {
       const scriptCode = document.getElementById("xe-script-input").value;
       const result = eval(scriptCode);
-      outputConsole(`Script executed successfully: ${result}`);
+      alert("Script executed successfully!");
+      console.log("Execution result:", result);
     } catch (error) {
-      outputConsole(`Error executing script: ${error.message}`);
+      alert("Error executing script: " + error.message);
     }
-  }
-
-  function outputConsole(message) {
-    const consoleOutput = document.getElementById("xe-script-console");
-    const outputText = document.createElement("div");
-    outputText.textContent = message;
-    consoleOutput.appendChild(outputText);
   }
 
   const container = document.createElement("div");
   container.style.position = "fixed";
-  container.style.top = "0";
-  container.style.left = "0";
-  container.style.right = "0";
+  container.style.top = "20px";
+  container.style.right = "20px";
   container.style.backgroundColor = "black";
   container.style.color = "green";
-  container.style.borderBottom = "2px solid green";
+  container.style.border = "2px solid green";
   container.style.padding = "10px";
+  container.style.borderRadius = "10px";
   container.style.zIndex = "9999";
 
   const input = document.createElement("textarea");
   input.id = "xe-script-input";
-  input.style.width = "100%";
+  input.style.width = "200px";
   input.style.height = "100px";
   input.placeholder = "Enter your script here...";
 
@@ -44,15 +38,8 @@
 
   executeButton.addEventListener("click", executeScript);
 
-  const consoleOutput = document.createElement("div");
-  consoleOutput.id = "xe-script-console";
-  consoleOutput.style.height = "150px";
-  consoleOutput.style.overflowY = "auto";
-  consoleOutput.style.borderTop = "1px solid green";
-
   container.appendChild(input);
   container.appendChild(executeButton);
-  container.appendChild(consoleOutput);
 
   document.body.appendChild(container);
 })();
